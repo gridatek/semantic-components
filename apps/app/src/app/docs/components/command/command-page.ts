a7878518213961b8fe7889ede9bc40fe263a92f0
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-09-04 16:32:03 +0200

    build: wip

--- apps/app/src/app/docs/components/command/command-page.ts
+++ apps/app/src/app/docs/components/command/command-page.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
 
-import { ScPage, ScPageContent, ScPageHeader, ScPageTitle } from '@semantic-components/ui';
-
 import { CommandDemoSection } from './command-demo-section';
-import { CommandDialogDemoSection } from './command-dialog-demo-section';
 
 @Component({
   selector: 'app-command-page',
-  imports: [
-    ScPage,
-    ScPageHeader,
-    ScPageTitle,
-    ScPageContent,
-    CommandDemoSection,
-    CommandDialogDemoSection,
-  ],
+  imports: [CommandDemoSection],
   template: `
-    <sc-page>
-      <sc-page-header>
-        <h1 sc-page-title>Command</h1>
-      </sc-page-header>
-      <sc-page-content>
-        <app-command-demo-section />
-        <app-command-dialog-demo-section />
-      </sc-page-content>
-    </sc-page>
+    <app-command-demo-section />
   `,
 })
 export default class CommandPage {}
