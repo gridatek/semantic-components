56c85e07963c1019fe2324ac9a7af3c9728891a0
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-09-04 13:20:00 +0200

    build: wip

--- apps/breadcrumb-showcase/src/app/app.config.ts
+++ apps/breadcrumb-showcase/src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(appRoutes),
+  ],
 };
