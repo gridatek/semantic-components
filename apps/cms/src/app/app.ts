cb031506f0defacccc1c703464903bd3ecde8914
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-09-04 11:54:42 +0200

    build: wip

---
+++ apps/cms/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  imports: [RouterModule],
+  selector: 'cms-root',
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  styles: ``,
+})
+export class App {
+  protected title = 'cms';
+}
