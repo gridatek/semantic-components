31d224642bdbbe54721578a267fc0a512a44cd6b
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-09-04 16:37:10 +0200

    build: wip

--- apps/app/src/app/docs/components/command/command-demo-section.ts
+++ apps/app/src/app/docs/components/command/command-demo-section.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 import { PreviewCodeTabs } from '../../../components/preview-code-tabs/preview-code-tabs';
 import { CommandDemo } from './command-demo';
@@ -7,11 +7,15 @@ import { CommandDemo } from './command-demo';
   selector: 'app-command-demo-section',
   imports: [PreviewCodeTabs, CommandDemo],
   template: `
-    <app-preview-code-tabs>
+    <app-preview-code-tabs [code]="code" [title]="title()" [level]="level()">
       <app-command-demo />
     </app-preview-code-tabs>
   `,
 })
 export class CommandDemoSection {
-  protected demoCode = ``;
+  readonly title = input<string>('');
+
+  readonly level = input<'2' | '3'>('2');
+
+  protected readonly code = ``;
 }
