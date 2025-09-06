9856a0fe32dbb7f0fc684158f0213a7467b0d048
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-09-06 10:33:39 +0200

    build: wip

--- libs/editor/src/lib/editor/actions/text-style/editor-font-size.ts
+++ libs/editor/src/lib/editor/actions/text-style/editor-font-size.ts
@@ -1,7 +1,6 @@
-import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation, inject } from '@angular/core';
 
-import { ScToggle, ScTooltip } from '@semantic-components/ui';
+import { ScMenu, ScMenuItem, ScMenuTriggerFor, ScToggle, ScTooltip } from '@semantic-components/ui';
 import { SiTypeIcon } from '@semantic-icons/lucide-icons';
 
 import { ScEditor } from '../../editor';
@@ -9,10 +8,10 @@ import { ScExtensions } from '../../extensions/extensions';
 
 @Component({
   selector: 'sc-editor-font-size',
-  imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, ScToggle, ScTooltip, SiTypeIcon],
+  imports: [ScMenuTriggerFor, ScMenu, ScMenuItem, ScToggle, ScTooltip, SiTypeIcon],
   template: `
     <button
-      [cdkMenuTriggerFor]="textSizeDropdown"
+      [scMenuTriggerFor]="fontSizeMenu"
       sc-toggle
       variant="outline"
       size="sm"
@@ -22,80 +21,14 @@ import { ScExtensions } from '../../extensions/extensions';
       <svg class="w-4 h-4" si-type-icon></svg>
     </button>
 
-    <ng-template #textSizeDropdown>
-      <div
-        class="z-10 w-72 rounded bg-white p-2 shadow dark:bg-gray-700"
-        id="textSizeDropdown"
-        cdkMenu
-      >
-        <ul class="space-y-1 text-sm font-medium" aria-labelledby="setTextSizeButton">
-          <li>
-            <button
-              class="flex w-full items-center justify-between rounded px-3 py-2 text-base text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
-              (cdkMenuItemTriggered)="setTextSize('16px')"
-              data-text-size="16px"
-              cdkMenuItem
-              type="button"
-            >
-              16px (Default)
-            </button>
-          </li>
-          <li>
-            <button
-              class="flex w-full items-center justify-between rounded px-3 py-2 text-xs text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
-              (cdkMenuItemTriggered)="setTextSize('12px')"
-              data-text-size="12px"
-              cdkMenuItem
-              type="button"
-            >
-              12px (Tiny)
-            </button>
-          </li>
-          <li>
-            <button
-              class="flex w-full items-center justify-between rounded px-3 py-2 text-sm text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
-              (cdkMenuItemTriggered)="setTextSize('14px')"
-              data-text-size="14px"
-              cdkMenuItem
-              type="button"
-            >
-              14px (Small)
-            </button>
-          </li>
-          <li>
-            <button
-              class="flex w-full items-center justify-between rounded px-3 py-2 text-lg text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
-              (cdkMenuItemTriggered)="setTextSize('18px')"
-              data-text-size="18px"
-              cdkMenuItem
-              type="button"
-            >
-              18px (Lead)
-            </button>
-          </li>
-          <li>
-            <button
-              class="flex w-full items-center justify-between rounded px-3 py-2 text-2xl text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
-              (cdkMenuItemTriggered)="setTextSize('24px')"
-              data-text-size="24px"
-              cdkMenuItem
-              type="button"
-            >
-              24px (Large)
-            </button>
-          </li>
-          <li>
-            <button
-              class="flex w-full items-center justify-between rounded px-3 py-2 text-4xl text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
-              (cdkMenuItemTriggered)="setTextSize('36px')"
-              data-text-size="36px"
-              cdkMenuItem
-              type="button"
-            >
-              36px (Huge)
-            </button>
-          </li>
-        </ul>
+    <ng-template #fontSizeMenu>
+      <div class="min-w-[12rem]" sc-menu>
+        <button class="text-base" (click)="setTextSize('16px')" sc-menu-item>16px (Default)</button>
+        <button class="text-xs" (click)="setTextSize('12px')" sc-menu-item>12px (Tiny)</button>
+        <button class="text-sm" (click)="setTextSize('14px')" sc-menu-item>14px (Small)</button>
+        <button class="text-lg" (click)="setTextSize('18px')" sc-menu-item>18px (Lead)</button>
+        <button class="text-2xl" (click)="setTextSize('24px')" sc-menu-item>24px (Large)</button>
+        <button class="text-4xl" (click)="setTextSize('36px')" sc-menu-item>36px (Huge)</button>
       </div>
     </ng-template>
   `,
