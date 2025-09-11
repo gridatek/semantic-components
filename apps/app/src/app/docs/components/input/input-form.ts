d9954b15e205e4d127aaf88838c1e66fe7920449
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-09-11 18:24:22 +0200

    build: wip

--- apps/app/src/app/docs/components/input/input-form.ts
+++ apps/app/src/app/docs/components/input/input-form.ts
@@ -23,6 +23,7 @@ import {
   ScToastTitle,
   Toaster,
 } from '@semantic-components/ui';
+import { SiXIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-input-form',
@@ -40,6 +41,7 @@ import {
     ScToastContent,
     ScToastTitle,
     ScToastClose,
+    SiXIcon,
   ],
   template: `
     <form [formGroup]="form" (ngSubmit)="onSubmit()">
@@ -76,18 +78,7 @@ import {
           </pre>
         </div>
         <button sc-toast-close>
-          <svg
-            class="h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <svg class="size-4" si-x-icon></svg>
         </button>
       </div>
     </ng-template>
