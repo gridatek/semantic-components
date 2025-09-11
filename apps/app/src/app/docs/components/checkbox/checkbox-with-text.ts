77bf9d2c0ef74f0ef5c0d3438a7d99b7699c072b
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-09-11 19:11:00 +0200

    build: wip

--- apps/app/src/app/docs/components/checkbox/checkbox-with-text.ts
+++ apps/app/src/app/docs/components/checkbox/checkbox-with-text.ts
@@ -1,23 +1,22 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
-import { ScCheckbox } from '@semantic-components/ui';
+import {
+  ScCheckbox,
+  ScField,
+  ScFieldDescription,
+  ScLabel,
+  ScLabelGroup,
+} from '@semantic-components/ui';
 
 @Component({
   selector: 'app-checkbox-with-text',
-  imports: [ScCheckbox],
+  imports: [ScCheckbox, ScFieldDescription, ScField, ScLabel, ScLabelGroup],
   template: `
-    <div class="items-top flex space-x-2">
-      <input id="terms1" sc-checkbox />
-      <div class="grid gap-1.5 leading-none">
-        <label
-          class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          for="terms1"
-        >
-          Accept terms and conditions
-        </label>
-        <p class="text-sm text-muted-foreground">
-          You agree to our Terms of Service and Privacy Policy.
-        </p>
+    <div sc-field orientation="horizontal-checkbox" controlId="terms1">
+      <input sc-checkbox />
+      <div sc-label-group>
+        <label sc-label>Accept terms and conditions</label>
+        <p sc-field-description>You agree to our Terms of Service and Privacy Policy.</p>
       </div>
     </div>
   `,
