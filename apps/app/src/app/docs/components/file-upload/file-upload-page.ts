4aefb5f3c60e462f4a37220878b559e7283fe3a0
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-09-18 10:07:17 +0200

    build: wip

--- apps/app/src/app/docs/components/file-upload/file-upload-page.ts
+++ apps/app/src/app/docs/components/file-upload/file-upload-page.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { FileUploadBasicDemoSection } from './file-upload-basic-demo-section';
 import { FileUploadMultipleDemoSection } from './file-upload-multiple-demo-section';
 import { FileUploadValidationDemoSection } from './file-upload-validation-demo-section';
+import { FileUploaderDemoSection } from './file-uploader-demo-section';
 
 @Component({
   selector: 'app-file-upload-page',
@@ -10,6 +11,7 @@ import { FileUploadValidationDemoSection } from './file-upload-validation-demo-s
     FileUploadBasicDemoSection,
     FileUploadMultipleDemoSection,
     FileUploadValidationDemoSection,
+    FileUploaderDemoSection,
   ],
   template: `
     <app-file-upload-basic-demo-section title="File Upload" />
@@ -17,6 +19,8 @@ import { FileUploadValidationDemoSection } from './file-upload-validation-demo-s
     <app-file-upload-multiple-demo-section title="Multiple Files" />
 
     <app-file-upload-validation-demo-section title="With Validation" />
+
+    <app-file-uploader-demo-section title="Cloud File Uploader" />
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
