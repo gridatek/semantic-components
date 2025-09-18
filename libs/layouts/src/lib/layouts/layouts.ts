10974e50787cc3a3ab41223d4f68625a95cdb43e
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-09-18 15:52:12 +0200

    build: wip

---
+++ libs/layouts/src/lib/layouts/layouts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sc-layouts',
+  imports: [],
+  templateUrl: './layouts.html',
+  styleUrl: './layouts.css',
+})
+export class Layouts {}
