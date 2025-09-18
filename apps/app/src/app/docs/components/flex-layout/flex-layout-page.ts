189aeaba189cf5b32312324b5e77ae1d38e82036
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-09-18 14:40:29 +0200

    build: wip

--- apps/app/src/app/docs/components/flex-layout/flex-layout-page.ts
+++ apps/app/src/app/docs/components/flex-layout/flex-layout-page.ts
@@ -4,6 +4,7 @@ import { FlexLayoutAlignmentSection } from './flex-layout-alignment-section';
 import { FlexLayoutDemoSection } from './flex-layout-demo-section';
 import { FlexLayoutDirectionSection } from './flex-layout-direction-section';
 import { FlexLayoutResponsiveSection } from './flex-layout-responsive-section';
+import { FlexLayoutSizingSection } from './flex-layout-sizing-section';
 
 @Component({
   selector: 'app-flex-layout-page',
@@ -11,6 +12,7 @@ import { FlexLayoutResponsiveSection } from './flex-layout-responsive-section';
     FlexLayoutDemoSection,
     FlexLayoutDirectionSection,
     FlexLayoutAlignmentSection,
+    FlexLayoutSizingSection,
     FlexLayoutResponsiveSection,
   ],
   template: `
@@ -19,6 +21,8 @@ import { FlexLayoutResponsiveSection } from './flex-layout-responsive-section';
     <app-flex-layout-direction-section />
 
     <app-flex-layout-alignment-section />
+
+    <app-flex-layout-sizing-section />
 
     <app-flex-layout-responsive-section />
   `,
