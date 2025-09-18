aada1b6b77d7952438b1e9548b7438423556ed1c
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-09-18 23:11:51 +0200

    build: wip

--- apps/cms/src/app/app.routes.ts
+++ apps/cms/src/app/app.routes.ts
@@ -13,9 +13,51 @@ export const appRoutes: Route[] = [
         path: 'about',
         loadComponent: () => import('./pages/about').then((m) => m.AboutPage),
       },
+      // CMS Pages
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard').then((m) => m.default),
+      },
+      {
+        path: 'articles',
+        loadComponent: () => import('./pages/articles').then((m) => m.default),
+      },
+      {
+        path: 'article-editor',
+        loadComponent: () => import('./pages/article-editor').then((m) => m.default),
+      },
+      {
+        path: 'settings',
+        loadComponent: () => import('./pages/settings').then((m) => m.default),
+      },
+      {
+        path: 'blog',
+        loadComponent: () => import('./pages/blog').then((m) => m.default),
+      },
+      // Layout Demos
+      {
+        path: 'demos',
+        children: [
+          {
+            path: 'center-layout',
+            loadComponent: () =>
+              import('./pages/layout-demos/center-layout-demo').then((m) => m.default),
+          },
+          {
+            path: 'split-layout',
+            loadComponent: () =>
+              import('./pages/layout-demos/split-layout-demo').then((m) => m.default),
+          },
+          {
+            path: '',
+            redirectTo: 'center-layout',
+            pathMatch: 'full',
+          },
+        ],
+      },
       {
         path: '',
-        redirectTo: 'home',
+        redirectTo: 'dashboard',
         pathMatch: 'full',
       },
     ],
