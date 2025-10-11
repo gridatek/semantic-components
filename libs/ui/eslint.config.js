7c7269e65a91af9d9f5f101dad846113000ffc49
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-10-11 14:11:27 +0200

    build: wip

--- libs/ui/eslint.config.js
+++ libs/ui/eslint.config.js
@@ -1,4 +1,3 @@
-const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
 
 module.exports = [
@@ -17,37 +16,5 @@ module.exports = [
     languageOptions: {
       parser: require('jsonc-eslint-parser'),
     },
-  },
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@angular-eslint/no-output-native': ['warn'],
-      '@angular-eslint/no-input-rename': ['warn'],
-      '@angular-eslint/component-class-suffix': 'off',
-      '@angular-eslint/directive-class-suffix': 'off',
-      '@angular-eslint/directive-selector': [
-        'warn',
-        {
-          type: 'attribute',
-          prefix: 'sc',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'warn',
-        {
-          type: 'element',
-          prefix: 'sc',
-          style: 'kebab-case',
-        },
-      ],
-    },
-  },
-  {
-    files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
   },
 ];
