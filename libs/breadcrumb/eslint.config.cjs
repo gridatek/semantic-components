1421bc08ec7d5fa9a08de5171d7d820200f1cb68
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-10-11 16:49:06 +0200

    build: wip

--- libs/breadcrumb/eslint.config.cjs
+++ libs/breadcrumb/eslint.config.cjs
@@ -1,4 +1,3 @@
-const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
 
 module.exports = [
@@ -16,33 +15,5 @@ module.exports = [
     languageOptions: {
       parser: require('jsonc-eslint-parser'),
     },
-  },
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'lib',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'lib',
-          style: 'kebab-case',
-        },
-      ],
-    },
-  },
-  {
-    files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
   },
 ];
