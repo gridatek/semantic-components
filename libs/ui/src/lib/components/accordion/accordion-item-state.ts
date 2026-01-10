1c0d088ad41b07dd9cef9aad328909363f2c0941
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-10 16:09:12 +0100

    build: wip

--- libs/ui/src/lib/components/accordion/accordion-item-state.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, linkedSignal, signal } from '@angular/core';
-
-@Injectable()
-export class ScAccordionItemState {
-  readonly open = signal(false);
-
-  readonly state = linkedSignal(() => (this.open() ? 'open' : 'closed'));
-}
--- libs/ui/src/lib/components/accordion/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './accordion';
-export * from './accordion-item';
-export * from './accordion-header';
-export * from './accordion-toggle';
-export * from './accordion-content';
