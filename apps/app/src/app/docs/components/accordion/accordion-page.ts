e0ab24e60ae9915c78eab937a5decb381ee09dc1
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-10 11:39:34 +0100

    build: wip

--- apps/app/src/app/docs/components/accordion/accordion-page.ts
+++ apps/app/src/app/docs/components/accordion/accordion-page.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 
 import { AccordionDemoSection } from './accordion-demo-section';
 import { NewAccordionDemoSection } from './new-accordion-demo-section';
+import { NewAccordionMultiDemoSection } from './new-accordion-multi-demo-section';
 
 @Component({
   selector: 'app-accordion-page',
-  imports: [AccordionDemoSection, NewAccordionDemoSection],
+  imports: [AccordionDemoSection, NewAccordionDemoSection, NewAccordionMultiDemoSection],
   template: `
     <app-accordion-demo-section />
-    <app-new-accordion-demo-section title="Angular ARIA Accordion" />
+    <app-new-accordion-demo-section title="Angular ARIA Accordion (Single Expansion)" />
+    <app-new-accordion-multi-demo-section title="Angular ARIA Accordion (Multi Expansion)" />
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
