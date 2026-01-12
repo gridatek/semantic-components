baed529df562e740b0f664396b728335c1e51fae
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-12 22:21:43 +0100

    build: wip

--- apps/app/src/app/docs/components/menu/aria-menu-demo.ts
+++ apps/app/src/app/docs/components/menu/aria-menu-demo.ts
@@ -49,11 +49,11 @@ import {
       cdkAttachPopoverAsChild
     >
       <div
-        class="z-50 min-w-[8rem] overflow-x-hidden overflow-y-auto rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+        class="z-50 min-w-[8rem] overflow-x-hidden overflow-y-auto rounded-md border bg-popover p-1 text-popover-foreground shadow-md"
         #formatMenu="ngMenu"
-        [attr.data-state]="trigger.expanded() ? 'open' : 'closed'"
-        [attr.data-side]="'bottom'"
         ngMenu
+        animate.enter="animate-in fade-in-0 zoom-in-95 slide-in-from-top-2"
+        animate.leave="animate-out fade-out-0 zoom-out-95 slide-out-to-top-2"
       >
         <ng-template ngMenuContent>
           <div
@@ -97,11 +97,11 @@ import {
             cdkAttachPopoverAsChild
           >
             <div
-              class="z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+              class="z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg"
               #categorizeMenu="ngMenu"
-              [attr.data-state]="formatMenu.visible() ? 'open' : 'closed'"
-              [attr.data-side]="'right'"
               ngMenu
+              animate.enter="animate-in fade-in-0 zoom-in-95 slide-in-from-left-2"
+              animate.leave="animate-out fade-out-0 zoom-out-95 slide-out-to-left-2"
             >
               <ng-template ngMenuContent>
                 <div
