e214bdab6677f1cde8a9472abac2c488731a31b6
kgridou <32600911+kgridou@users.noreply.github.com>
2026-01-16 00:49:56 +0100

    build: wip

--- apps/app/src/app/docs/components/select/select-demo.ts
+++ apps/app/src/app/docs/components/select/select-demo.ts
@@ -5,13 +5,14 @@ import {
   ComboboxPopupContainer,
 } from '@angular/aria/combobox';
 import { Listbox, Option } from '@angular/aria/listbox';
-import { CdkConnectedOverlay } from '@angular/cdk/overlay';
+import { OverlayModule } from '@angular/cdk/overlay';
 import {
   ChangeDetectionStrategy,
   Component,
   ViewEncapsulation,
   afterRenderEffect,
   computed,
+  signal,
   viewChild,
   viewChildren,
 } from '@angular/core';
@@ -23,54 +24,52 @@ import { SiCheckIcon, SiChevronDownIcon } from '@semantic-icons/lucide-icons';
   imports: [
     Combobox,
     ComboboxInput,
+    ComboboxPopup,
     ComboboxPopupContainer,
     Listbox,
     Option,
-    CdkConnectedOverlay,
+    OverlayModule,
     SiChevronDownIcon,
     SiCheckIcon,
   ],
   template: `
     <div ngCombobox readonly>
-      <button
-        class="border-input data-[placeholder]:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 dark:hover:bg-input/50 flex w-full items-center justify-between gap-2 rounded-md border bg-transparent px-3 py-2 text-sm whitespace-nowrap shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 h-9 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4"
+      <div
+        class="border-input focus-within:border-ring focus-within:ring-ring/50 flex h-9 w-full items-center relative rounded-md border bg-transparent text-sm shadow-xs transition-[color,box-shadow] focus-within:ring-[3px] [&:has(input[aria-disabled=true])]:cursor-not-allowed [&:has(input[aria-disabled=true])]:opacity-50"
         #origin
-        type="button"
+        data-slot="select-trigger"
       >
         <span
-          class="line-clamp-1 flex items-center gap-2 pointer-events-none"
-          data-slot="select-value"
+          class="line-clamp-1 flex flex-1 items-center gap-2 pointer-events-none left-3 absolute"
         >
           {{ displayValue() }}
         </span>
         <input
-          class="sr-only"
+          class="absolute inset-0 cursor-pointer opacity-0 px-9 h-full border-none"
           aria-label="Select dropdown"
           placeholder="Select an option"
           ngComboboxInput
         />
         <svg
-          class="size-4 opacity-50 text-muted-foreground transition-transform duration-150"
+          class="size-4 opacity-50 text-muted-foreground transition-transform duration-150 pointer-events-none absolute right-3"
           [class.rotate-180]="combobox()?.expanded()"
           si-chevron-down-icon
         ></svg>
-      </button>
+      </div>
       <ng-template ngComboboxPopupContainer>
         <ng-template
-          [cdkConnectedOverlayOrigin]="origin"
+          [cdkConnectedOverlay]="{ origin, usePopover: 'inline', matchWidth: true }"
           [cdkConnectedOverlayOpen]="true"
-          [cdkConnectedOverlayWidth]="origin.offsetWidth"
-          cdkConnectedOverlay
         >
           <div
-            class="bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border shadow-md mt-1"
+            class="bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 relative z-50 min-w-[8rem] overflow-hidden rounded-md border shadow-md mt-1 p-1"
             [attr.data-state]="combobox()?.expanded() ? 'open' : 'closed'"
             data-slot="select-content"
           >
-            <div class="p-1 overflow-y-auto max-h-60" ngListbox>
+            <div class="flex flex-col gap-0.5 overflow-y-auto max-h-60" ngListbox>
               @for (label of labels; track label.value) {
                 <div
-                  class="focus:bg-accent focus:text-accent-foreground data-[active=true]:bg-accent data-[active=true]:text-accent-foreground relative flex w-full cursor-default items-center gap-2 rounded-sm py-1.5 pr-8 pl-2 text-sm outline-none select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4"
+                  class="data-[active=true]:bg-accent data-[active=true]:text-accent-foreground [&[aria-selected=true]]:bg-accent [&[aria-selected=true]]:text-accent-foreground relative flex w-full cursor-default items-center rounded-sm py-1.5 pr-8 pl-2 text-sm outline-none select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
                   [value]="label.value"
                   [label]="label.value"
                   data-slot="select-item"
@@ -78,12 +77,10 @@ import { SiCheckIcon, SiChevronDownIcon } from '@semantic-icons/lucide-icons';
                 >
                   <span class="flex-1">{{ label.value }}</span>
                   <span
-                    class="absolute right-2 flex size-3.5 items-center justify-center"
+                    class="absolute right-2 flex size-3.5 items-center justify-center [div:not([aria-selected=true])_&]:hidden"
                     data-slot="select-item-indicator"
                   >
-                    @if (isSelected(label.value)) {
-                      <svg class="size-4" si-check-icon></svg>
-                    }
+                    <svg class="size-4" si-check-icon></svg>
                   </span>
                 </div>
               }
@@ -114,12 +111,6 @@ export class SelectDemo {
     return values.length ? values[0] : 'Select an option';
   });
 
-  /** Check if a value is selected */
-  isSelected(value: string): boolean {
-    const values = this.listbox()?.values() || [];
-    return values.includes(value);
-  }
-
   /** The options that are available for selection. */
   labels = [
     { value: 'Apple' },
@@ -131,6 +122,7 @@ export class SelectDemo {
 
   constructor() {
     // Scrolls to the active item when the active option changes.
+    // The slight delay here is to ensure animations are done before scrolling.
     afterRenderEffect(() => {
       const option = this.options().find((opt) => opt.active());
       setTimeout(() => option?.element.scrollIntoView({ block: 'nearest' }), 50);
