6c5099b79c2022122c8475adf2d1e424bd02fe3f
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-15 17:55:43 +0100

    wip

--- libs/ui/src/lib/components/alert-dialog/alert-dialog.ts
+++ libs/ui/src/lib/components/alert-dialog/alert-dialog.ts
@@ -52,10 +52,9 @@ export class ScAlertDialog {
 
   protected readonly class = computed(() =>
     cn(
-      'bg-background ring-foreground/10 gap-4 rounded-xl p-4 ring-1 duration-100 outline-none',
+      'relative bg-background ring-foreground/10 grid w-full max-w-[calc(100%-2rem)] gap-4 rounded-xl p-4 text-sm ring-1 duration-100 outline-none',
       'data-[size=default]:max-w-xs data-[size=sm]:max-w-xs data-[size=default]:sm:max-w-sm',
       'group/alert-dialog-content',
-      'fixed top-1/2 left-1/2 z-50 grid w-full -translate-x-1/2 -translate-y-1/2',
       'data-idle:opacity-0',
       'data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95',
       'data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95',
