f3a1140d6203b04615c6180dcf8e488e0e584092
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-20 21:08:02 +0100

    wip

--- libs/ui/src/lib/components/date-picker/date-picker.ts
+++ libs/ui/src/lib/components/date-picker/date-picker.ts
@@ -15,6 +15,7 @@ import {
   ScPopoverAlign,
   ScPopoverSide,
 } from '../popover';
+import { SiCalendarIcon } from '@semantic-icons/lucide-icons';
 import { Temporal } from '@js-temporal/polyfill';
 import {
   ScCalendar,
@@ -31,27 +32,12 @@ import {
     ScPopoverPortal,
     ScPopover,
     ScCalendar,
+    SiCalendarIcon,
   ],
   template: `
     <div scPopoverProvider [(open)]="open" [side]="side()" [align]="align()">
       <button scPopoverTrigger type="button" [class]="triggerClass()">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="mr-2 size-4"
-        >
-          <path d="M8 2v4" />
-          <path d="M16 2v4" />
-          <rect width="18" height="18" x="3" y="4" rx="2" />
-          <path d="M3 10h18" />
-        </svg>
+        <svg siCalendarIcon class="mr-2 size-4"></svg>
         <span [class]="displayText() ? '' : 'text-muted-foreground'">
           {{ displayText() || placeholder() }}
         </span>
