f3fb0a014d3c4e9c8eca629bc6ca80290c867309
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-21 19:52:46 +0100

    wip

---
+++ libs/ui/src/lib/components/menu-bar/menu-bar-item.ts
@@ -0,0 +1,85 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  contentChild,
+  effect,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { CdkOverlayOrigin, OverlayModule } from '@angular/cdk/overlay';
+import { MenuItem } from '@angular/aria/menu';
+import { SIGNAL, signalSetFn } from '@angular/core/primitives/signals';
+import { cn } from '../../utils';
+import { ScMenuPortal } from '../menu';
+import { ScMenuBar } from './menu-bar';
+
+@Component({
+  selector: '[scMenuBarItem]',
+  exportAs: 'scMenuBarItem',
+  imports: [OverlayModule, NgTemplateOutlet],
+  hostDirectives: [
+    {
+      directive: MenuItem,
+      inputs: ['id', 'value', 'disabled'],
+    },
+    CdkOverlayOrigin,
+  ],
+  template: `
+    <ng-content />
+
+    @if (submenuPortal(); as portal) {
+      <ng-template
+        [cdkConnectedOverlayOpen]="rendered()"
+        [cdkConnectedOverlay]="{ origin: overlayOrigin, usePopover: 'inline' }"
+        [cdkConnectedOverlayPositions]="[
+          {
+            originX: 'start',
+            originY: 'bottom',
+            overlayX: 'start',
+            overlayY: 'top',
+            offsetY: 4,
+          },
+        ]"
+        cdkAttachPopoverAsChild
+      >
+        <ng-container [ngTemplateOutlet]="portal.templateRef" />
+      </ng-template>
+    }
+  `,
+  host: {
+    'data-slot': 'menu-bar-item',
+    '[class]': 'class()',
+  },
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScMenuBarItem {
+  readonly classInput = input<string>('', { alias: 'class' });
+  readonly overlayOrigin = inject(CdkOverlayOrigin);
+  private readonly menuItem = inject(MenuItem);
+  protected readonly submenuOpen = computed(() => !!this.menuItem.expanded());
+  protected readonly submenuPortal = contentChild(ScMenuPortal);
+
+  private readonly scMenuBar = inject(ScMenuBar);
+
+  protected readonly rendered = computed(() => this.scMenuBar.rendered());
+
+  protected readonly class = computed(() =>
+    cn(
+      'flex items-center rounded-sm px-2 py-0.5 text-sm font-medium outline-hidden select-none cursor-default hover:bg-muted aria-expanded:bg-muted focus-visible:outline-2 focus-visible:outline-ring',
+      this.classInput(),
+    ),
+  );
+
+  constructor() {
+    effect(() => {
+      const menu = this.submenuPortal()?.menu();
+      if (menu) {
+        signalSetFn(this.menuItem.submenu[SIGNAL], menu);
+      }
+    });
+  }
+}
---
+++ libs/ui/src/lib/components/menu-bar/menu-bar.ts
@@ -0,0 +1,29 @@
+import { computed, Directive, input, signal } from '@angular/core';
+import { MenuBar } from '@angular/aria/menu';
+import { cn } from '../../utils';
+
+@Directive({
+  selector: '[scMenuBar]',
+  hostDirectives: [MenuBar],
+  host: {
+    'data-slot': 'menu-bar',
+    '[class]': 'class()',
+    '(focusin)': 'onFocusIn()',
+  },
+})
+export class ScMenuBar {
+  readonly classInput = input<string>('', { alias: 'class' });
+
+  rendered = signal(false);
+
+  protected readonly class = computed(() =>
+    cn(
+      'flex h-8 items-center gap-0.5 rounded-lg border bg-background p-[3px]',
+      this.classInput(),
+    ),
+  );
+
+  onFocusIn() {
+    this.rendered.set(true);
+  }
+}
