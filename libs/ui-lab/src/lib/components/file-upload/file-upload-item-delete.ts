285538ced0017d1cf08ec4e97a921d924d06fc01
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-24 00:30:02 +0100

    wip

--- libs/ui-lab/src/lib/components/file-upload/file-upload-item-delete.ts
+++ libs/ui-lab/src/lib/components/file-upload/file-upload-item-delete.ts
@@ -7,26 +7,14 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { buttonVariants, cn } from '@semantic-components/ui';
+import { SiXIcon } from '@semantic-icons/lucide-icons';
 import { SC_FILE_UPLOAD } from './file-upload';
 
 @Component({
   selector: 'button[scFileUploadItemDelete]',
+  imports: [SiXIcon],
   template: `
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="size-4"
-    >
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
-    </svg>
+    <svg siXIcon></svg>
     <span class="sr-only">Remove file</span>
   `,
   host: {
