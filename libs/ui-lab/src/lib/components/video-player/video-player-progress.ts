489cc2958c840e70b1022fcb66e6e364492257ea
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-27 20:08:06 +0100

    wip

--- libs/ui-lab/src/lib/components/video-player/video-player-progress.ts
+++ libs/ui-lab/src/lib/components/video-player/video-player-progress.ts
@@ -15,13 +15,13 @@ import { SC_VIDEO_PLAYER } from './video-player';
   template: `
     <!-- Buffered -->
     <div
-      class="pointer-events-none absolute top-1/2 left-0 h-1 -translate-y-1/2 rounded-full bg-white/50"
+      class="pointer-events-none absolute top-1/2 left-0 h-1 -translate-y-1/2 rounded-full bg-white/50 transition-[height] group-hover:h-1.5"
       [style.width.%]="player.bufferedPercent()"
     ></div>
     <!-- Seek slider -->
     <input
       scSlider
-      class="absolute inset-0 h-full"
+      class="absolute inset-0 h-full [&::-moz-range-progress]:transition-[height] group-hover:[&::-moz-range-progress]:h-1.5 [&::-moz-range-track]:transition-[height] group-hover:[&::-moz-range-track]:h-1.5 [&::-webkit-slider-runnable-track]:transition-[height] group-hover:[&::-webkit-slider-runnable-track]:h-1.5"
       min="0"
       max="100"
       step="0.1"
@@ -44,7 +44,7 @@ export class ScVideoPlayerProgress {
   readonly classInput = input<string>('', { alias: 'class' });
 
   protected readonly class = computed(() =>
-    cn('relative mb-2 block h-3', this.classInput()),
+    cn('group relative mb-2 block h-3', this.classInput()),
   );
 
   protected onSeek(event: Event): void {
