7c2f2a8e205bb9cb2acf8acbf79d66e5831c35dc
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-27 19:05:24 +0100

    wip

--- libs/ui/src/lib/components/resizable/resizable-handle-indicator.ts
+++ libs/ui/src/lib/components/resizable/resizable-handle-indicator.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
+import { computed } from 'node_modules/@angular/aria/types/_list-navigation-chunk';
+import { cn } from '../../utils';
 
 @Directive({
   selector: '[scResizableHandleIndicator]',
   host: {
     'data-slot': 'resizable-handle-indicator',
-    class: 'bg-border z-10 flex h-6 w-1 shrink-0 rounded-lg',
+    '[class]': 'class()',
   },
 })
-export class ScResizableHandleIndicator {}
+export class ScResizableHandleIndicator {
+  readonly classInput = input<string>('', { alias: 'class' });
+
+  protected readonly class = computed(() =>
+    cn('bg-border z-10 flex h-6 w-1 shrink-0 rounded-lg', this.classInput()),
+  );
+}
