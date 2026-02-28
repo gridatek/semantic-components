f99fc53cce677f554cba5be9bf1ee72accb4f763
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-01 00:02:01 +0100

    wip

--- libs/ui-lab/src/lib/components/image-compare/index.ts
+++ libs/ui-lab/src/lib/components/image-compare/index.ts
@@ -1,5 +1,5 @@
 export { ScImageCompare, SC_IMAGE_COMPARE } from './image-compare';
-export { ScImageCompareArea } from './image-compare-container';
+export { ScImageCompareArea } from './image-compare-area';
 export { ScImageCompareBefore } from './image-compare-before';
 export { ScImageCompareAfter } from './image-compare-after';
 export { ScImageCompareSlider } from './image-compare-slider';
