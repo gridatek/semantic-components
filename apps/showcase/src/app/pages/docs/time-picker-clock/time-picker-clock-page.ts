8defe25a1bd5cd8eb46caf4d4efdd5b8a0901263
Khalil LAGRIDA <khalil.lagrida@gmail.com>
2026-03-01 11:06:11 +0100

    wip

--- apps/showcase/src/app/pages/docs/time-picker-clock/time-picker-clock-page.ts
+++ apps/showcase/src/app/pages/docs/time-picker-clock/time-picker-clock-page.ts
@@ -4,7 +4,6 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { HoursTimePickerClockDemoContainer } from './demos/hours-time-picker-clock-demo-container';
-import { MinutesTimePickerClockDemoContainer } from './demos/minutes-time-picker-clock-demo-container';
 import { TocHeading } from '../../../components/toc/toc-heading';
 import { ComponentBadges } from '../../../components/component-badges/component-badges';
 
@@ -14,7 +13,6 @@ import { ScHeading } from '@semantic-components/ui';
   selector: 'app-time-picker-clock-page',
   imports: [
     HoursTimePickerClockDemoContainer,
-    MinutesTimePickerClockDemoContainer,
     TocHeading,
     ComponentBadges,
     ScHeading,
@@ -32,7 +30,6 @@ import { ScHeading } from '@semantic-components/ui';
       <section class="space-y-8">
         <h2 scHeading toc>Examples</h2>
         <app-hours-time-picker-clock-demo-container />
-        <app-minutes-time-picker-clock-demo-container />
       </section>
     </div>
   `,
