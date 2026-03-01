0970a670cd3ba73d78b2c084872373831aa19d49
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-01 17:03:06 +0100

    wip

--- apps/blocks/src/app/pages/love-letter/love-letter.page.ts
+++ apps/blocks/src/app/pages/love-letter/love-letter.page.ts
@@ -30,7 +30,7 @@ import { PlayerHand } from './player-hand';
     ScCardTitle,
   ],
   template: `
-    <div class="flex min-h-screen flex-col">
+    <div class="flex min-h-screen flex-col lg:h-screen lg:overflow-hidden">
       <!-- Header -->
       <div class="border-b px-4 py-2">
         <div class="flex items-center justify-between">
@@ -39,9 +39,9 @@ import { PlayerHand } from './player-hand';
         </div>
       </div>
 
-      <div class="flex flex-1 flex-col lg:flex-row">
+      <div class="flex min-h-0 flex-1 flex-col lg:flex-row">
         <!-- Left: Players -->
-        <div class="lg:w-64 lg:shrink-0 lg:border-r">
+        <div class="lg:w-64 lg:shrink-0 lg:overflow-y-auto lg:border-r">
           <app-player-grid
             [players]="players"
             [youPlayerId]="youPlayerId"
@@ -50,7 +50,7 @@ import { PlayerHand } from './player-hand';
         </div>
 
         <!-- Center: Game Area -->
-        <div class="flex flex-1 flex-col justify-between p-4">
+        <div class="flex flex-1 flex-col justify-between overflow-y-auto p-4">
           <!-- Message -->
           <div scCard class="mb-4">
             <div scCardBody>
@@ -185,7 +185,9 @@ import { PlayerHand } from './player-hand';
         </div>
 
         <!-- Right: Log (desktop only) -->
-        <div class="hidden lg:block lg:w-64 lg:shrink-0 lg:border-l lg:p-3">
+        <div
+          class="hidden lg:block lg:w-64 lg:shrink-0 lg:overflow-y-auto lg:border-l lg:p-3"
+        >
           <div scCard>
             <div scCardHeader>
               <h3 scCardTitle>Game Log</h3>
