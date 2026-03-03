fbdfe32aed5902e5ed297d9b6ead0f7ae11c7fbb
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-03 01:38:29 +0100

    wip

--- libs/ui/src/lib/components/copy-to-clipboard/copy-to-clipboard.ts
+++ libs/ui/src/lib/components/copy-to-clipboard/copy-to-clipboard.ts
@@ -1,5 +1,5 @@
 import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
-import { Directive, inject } from '@angular/core';
+import { Directive, inject, input } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { concat, delay, filter, of, switchMap } from 'rxjs';
 
@@ -16,10 +16,12 @@ import { concat, delay, filter, of, switchMap } from 'rxjs';
 export class ScCopyToClipboard {
   private readonly cdkCopyToClipboard = inject(CdkCopyToClipboard);
 
+  readonly duration = input(2000);
+
   readonly copied = toSignal(
     this.cdkCopyToClipboard.copied.pipe(
       filter((success) => success),
-      switchMap(() => concat(of(true), of(false).pipe(delay(2000)))),
+      switchMap(() => concat(of(true), of(false).pipe(delay(this.duration())))),
     ),
     { initialValue: false },
   );
