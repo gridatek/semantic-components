760c41de656e9c5796114bbc97d1c7d76cd22e6a
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-04 21:30:02 +0100

    wip

--- libs/ui/src/lib/utilities/auto-select/auto-select.ts
+++ libs/ui/src/lib/utilities/auto-select/auto-select.ts
@@ -1,5 +1,6 @@
-import { FocusMonitor, FocusOrigin } from '@angular/cdk/a11y';
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { DestroyRef, Directive, ElementRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Directive({
   selector: 'input[scAutoSelect]',
@@ -9,16 +10,16 @@ export class ScAutoSelect {
   private readonly elementRef = inject(ElementRef<HTMLInputElement>);
 
   constructor() {
-    const subscription = this.focusMonitor
+    this.focusMonitor
       .monitor(this.elementRef, false)
-      .subscribe((origin: FocusOrigin) => {
+      .pipe(takeUntilDestroyed())
+      .subscribe((origin) => {
         if (origin === 'program' && this.elementRef.nativeElement.value) {
           this.elementRef.nativeElement.select();
         }
       });
 
     inject(DestroyRef).onDestroy(() => {
-      subscription.unsubscribe();
       this.focusMonitor.stopMonitoring(this.elementRef);
     });
   }
