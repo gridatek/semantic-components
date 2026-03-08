f6a54a9dfc076b28771c5ffefc35fc9c0075ffcd
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-08 02:02:57 +0100

    wip

--- libs/ui/src/lib/components/breadcrumb/breadcrumb.ts
+++ libs/ui/src/lib/components/breadcrumb/breadcrumb.ts
@@ -1,12 +1,9 @@
-import { Directive, input } from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'nav[scBreadcrumb]',
   host: {
     'data-slot': 'breadcrumb',
-    '[attr.aria-label]': 'ariaLabel()',
   },
 })
-export class ScBreadcrumb {
-  readonly ariaLabel = input('breadcrumb', { alias: 'aria-label' });
-}
+export class ScBreadcrumb {}
