7f81d3ab95e917a46cebf9158c0498ae0d721de1
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-13 15:39:33 +0100

    wip

--- libs/ui/src/lib/components/combobox/combobox-origin.ts
+++ libs/ui/src/lib/components/combobox/combobox-origin.ts
@@ -26,10 +26,5 @@ export class ScComboboxOrigin {
   readonly elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
   readonly classInput = input<string>('', { alias: 'class' });
 
-  protected readonly class = computed(() =>
-    cn(
-      'border-input dark:bg-input/30 dark:hover:bg-input/50 focus-visible:border-ring focus-visible:ring-ring/50 relative flex h-8 w-full items-center gap-1.5 rounded-lg border bg-transparent py-2 pr-2 pl-2.5 text-sm whitespace-nowrap transition-colors select-none outline-none focus-visible:ring-3 has-aria-disabled:cursor-not-allowed has-aria-disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
-      this.classInput(),
-    ),
-  );
+  protected readonly class = computed(() => cn('block', this.classInput()));
 }
