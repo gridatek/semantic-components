a833d264c2f78259a861050911f2dab05bf4e122
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-13 18:43:58 +0100

    wip

--- libs/ui/src/lib/components/input-group/input-group-button.ts
+++ libs/ui/src/lib/components/input-group/input-group-button.ts
@@ -9,7 +9,7 @@ const inputGroupButtonVariants = cva(
     variants: {
       size: {
         xs: "h-6 gap-1 rounded-[calc(var(--radius)-3px)] px-1.5 [&>svg:not([class*='size-'])]:size-3.5",
-        sm: '',
+        sm: "h-7 gap-1 rounded-[calc(var(--radius)-3px)] px-2.5 text-[0.8rem] [&>svg:not([class*='size-'])]:size-3.5",
         'icon-xs':
           'size-6 rounded-[calc(var(--radius)-3px)] p-0 has-[>svg]:p-0',
         'icon-sm': 'size-8 p-0 has-[>svg]:p-0',
