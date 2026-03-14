275ccb7c23217eb027c051b2c88333733c622f4b
Khalil LAGRIDA <khalil.lagrida@gmail.com>
2026-03-14 11:45:38 +0100

    wip

--- apps/showcase/src/app/pages/docs/button-group/demos/button-group-split-demo-container.ts
+++ apps/showcase/src/app/pages/docs/button-group/demos/button-group-split-demo-container.ts
@@ -23,33 +23,5 @@ import { ButtonGroupSplitDemo } from './button-group-split-demo';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonGroupSplitDemoContainer {
-  readonly code = `import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
-import {
-  ScButton,
-  ScButtonGroup,
-  ScButtonGroupSeparator,
-} from '@semantic-components/ui';
-import { SiChevronDownIcon } from '@semantic-icons/lucide-icons';
-
-@Component({
-  selector: 'app-button-group-split-demo',
-  imports: [ScButton, ScButtonGroup, ScButtonGroupSeparator, SiChevronDownIcon],
-  host: { class: 'flex w-full justify-center' },
-  encapsulation: ViewEncapsulation.None,
-  template: \`
-    <div scButtonGroup>
-      <button scButton variant="outline">Save</button>
-      <div scButtonGroupSeparator></div>
-      <button scButton variant="outline" size="icon" aria-label="More options">
-        <svg siChevronDownIcon></svg>
-      </button>
-    </div>
-  \`,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ButtonGroupSplitDemo {}`;
+  readonly code = '';
 }
