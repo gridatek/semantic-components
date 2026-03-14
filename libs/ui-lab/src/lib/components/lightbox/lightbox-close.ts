1847ea568b1f0cfda7e90e985d51c4ee7c37e04e
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-14 23:36:50 +0100

    wip

--- libs/ui-lab/src/lib/components/lightbox/lightbox-close.ts
+++ libs/ui-lab/src/lib/components/lightbox/lightbox-close.ts
@@ -1,5 +1,5 @@
 import { Directive, computed, inject, input } from '@angular/core';
-import { cn } from '@semantic-components/ui';
+import { buttonVariants, cn } from '@semantic-components/ui';
 import { SC_LIGHTBOX_PROVIDER } from './lightbox-provider';
 
 @Directive({
@@ -17,7 +17,8 @@ export class ScLightboxClose {
 
   protected readonly class = computed(() =>
     cn(
-      'absolute top-4 right-4 z-10 rounded-full p-2 text-white/80 transition-colors hover:bg-white/10 hover:text-white [&>svg]:size-6',
+      buttonVariants({ variant: 'ghost', size: 'icon' }),
+      "absolute top-4 right-4 z-10 size-10 rounded-full text-white/80 hover:bg-white/10 hover:text-white [&_svg:not([class*='size-'])]:size-6",
       this.classInput(),
     ),
   );
