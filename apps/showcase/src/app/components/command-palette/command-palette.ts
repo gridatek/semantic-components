7ec3329eb0936942aa7a671cf7aa35be85f3eaa7
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-15 14:46:20 +0100

    wip

--- apps/showcase/src/app/components/command-palette/command-palette.ts
+++ apps/showcase/src/app/components/command-palette/command-palette.ts
@@ -65,7 +65,7 @@ import { ComponentsService } from '../../services/components.service';
       ></span>
       <ng-template scDialogPortal>
         <div scDialog class="w-lg gap-0 p-0">
-          <div scCommand class="**:data-[slot=command-input-group]:h-12">
+          <div scCommand>
             <div scCommandInputGroup>
               <svg
                 siSearchIcon
