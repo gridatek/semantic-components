de8bb03116d7d763826f9427152c3f0405498c3c
kgridou <32600911+kgridou@users.noreply.github.com>
2026-03-16 06:40:01 +0100

    wip

--- apps/showcase/src/app/pages/docs/virtual-list/demos/basic-virtual-list-demo.ts
+++ apps/showcase/src/app/pages/docs/virtual-list/demos/basic-virtual-list-demo.ts
@@ -21,6 +21,7 @@ import {
     <cdk-virtual-scroll-viewport
       itemSize="48"
       class="h-[300px] rounded-lg border"
+      tabindex="0"
     >
       <div
         *cdkVirtualFor="let item of items(); let i = index"
